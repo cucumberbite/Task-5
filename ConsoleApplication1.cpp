1fe124e188ae2eb05864808e5a1b20b351a6e7b2
cucumberbite <35292376+cucumberbite@users.noreply.github.com>
2020-04-14 16:05:36 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+	setlocale(0, "");
+	double x, y_0, y_1 = 0;
+	cout << "Введите число, из которого требуется вычислить кубический корень: ";
+	cin >> x;
+	y_0 = x;
+	while (true) {
+		y_1 = 0.5 * (y_0 + 3 * x / (2 * y_0 * y_0 + x / y_0));
+			if (abs(y_1 - y_0) < pow(10, -5))
+				break;
+		y_0 = y_1;
+	}
+	cout << "Куб. корень: " << y_0 << endl;
+	return 0;
+}
